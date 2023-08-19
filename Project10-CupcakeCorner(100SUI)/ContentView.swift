2d221b5924505b5db9d7d4906f5f16ab0b91fd81
Artem <moiseev@physics.msu.ru>
2023-08-19 17:13:31 +0300

    Initial Commit

---
+++ Project10-CupcakeCorner(100SUI)/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Project10-CupcakeCorner(100SUI)
+//
+//  Created by Artem on 19.08.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project10-CupcakeCorner(100SUI)/Project10_CupcakeCorner_100SUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  Project10_CupcakeCorner_100SUI_App.swift
+//  Project10-CupcakeCorner(100SUI)
+//
+//  Created by Artem on 19.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Project10_CupcakeCorner_100SUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
